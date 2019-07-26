291a35999a0272f11f655c476575147aaeb7b157
Daniel Brackett <gerard.brackett@gmail.com>
2019-07-25 17:14:06 -0700

    Added a get Deck Size method, a shuffle method and a create Deck method. Additionally I added a String [] to hold the suit names. I added some class level varisbales not sure if this makes sense. Not sure that it make sense for the 'deck to create itself' outside of the constructor.

--- Deck.java
+++ Deck.java
@@ -1,13 +1,46 @@
 import java.util.ArrayList;
+import java.util.Random;
 
-class Deck{
 
-    public Deck(){
-        ArrayList<Cards> deck = new ArrayList<Cards>();
+public class Deck{
+
+    public Deck deck;
+    public String[] SUITS = {"Clubs", "Diamonds", "Hearts", "Spades"};
+    private ArrayList<Card> cards = new ArrayList<>(52);
+
+    public Deck() {
+        deck.createDeck();
     }
 
+    public Deck shuffle() {
+        for (int i = 0; i < cards.size(); i++) {
+            Card currentCard = cards.get(i);
+            Random rand = new Random();
+            int value = rand.nextInt(86317389);
+            int swapIndex = value % this.getDeckSize();
+            Card temp = cards.get(swapIndex);
+            cards.set(swapIndex, currentCard);
+            cards.set(i, temp);
+        }
+        return deck;
     }
 
+    /*
+    make one card of each value(1-13) for each suit.
+    iterate over the suits and for each one make a value 1-13
+    */
+    public ArrayList<Card> createDeck() {
+        for (String suitName : SUITS) {
+            for (int i = 1; i <= 13; i++) {
+                Card c = new Card(suitName, i);
+                cards.add(c);
+            }
+        }
+        return cards;
+    }
 
+    public int getDeckSize() {
+        return cards.size();
+    }
 
 }
