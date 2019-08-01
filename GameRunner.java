600e4c74ae54447935e7c1f6837578285abae732
Daniel Brackett <gerard.brackett@gmail.com>
2019-07-31 18:55:32 -0700

    added a line to print to the terminal.

--- GameRunner.java
+++ GameRunner.java
@@ -1,9 +1,10 @@
 public class GameRunner {
+//may split this up this to make it a black jack runner and a general game runner.
 
     public static void main(String[] args) {
 
         Deck deck = new Deck();
         deck.shuffle();
-
+        deck.printDeck();
     }
 }
