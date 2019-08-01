0e55e3ece8d9e9444264720e5ca03e083ef49ab5
Daniel Brackett <gerard.brackett@gmail.com>
2019-08-01 16:20:31 -0700

    Moved that value of the cards and contents of the Hand to be visible to the GameRunner. added a method to return the current number of cards in any given hand.

--- Hand.java
+++ Hand.java
@@ -3,15 +3,19 @@ import java.util.ArrayList;
 public class Hand {
 
     public int valueOfCards;
+    public ArrayList<Card> cardsInHand;
 
     public Hand() {
-        boolean isDealer; //necessary if there is a tie between hands, the dealer wins.
-        ArrayList<Card> cardsInHand = new ArrayList<>(); //cards dealt to a player not to exceed 21.
+        cardsInHand = new ArrayList<>(); //cards dealt to a player not to exceed 21.
     }
 
     public int valueOfCardsinHand() {
 
+
         return valueOfCards;
     }
 
+    public int getNumberOfCardsInHand() {
+        return  cardsInHand.size();
+    }
 }
