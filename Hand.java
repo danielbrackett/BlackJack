6a96a0aeb27c57eb7546090504211a70542ea76b
Daniel Brackett <gerard.brackett@gmail.com>
2019-08-02 13:59:20 -0700

    Adding a 'addCardToHand()' returns void, 'printCardsInHand() returns void, getHandSize() returns int.

--- Hand.java
+++ Hand.java
@@ -9,13 +9,21 @@ public class Hand {
         cardsInHand = new ArrayList<>(); //cards dealt to a player not to exceed 21.
     }
 
+    public void addCardToHand(Card card) {
+        this.cardsInHand.add(card);
+    }
+
     public int valueOfCardsinHand() {
-
-
         return valueOfCards;
     }
 
-    public int getNumberOfCardsInHand() {
+    public void printCardsInHand() {
+        for (Card card: cardsInHand) {
+            System.out.println(card.ToString());
+        }
+    }
+
+    public int getHandSize() {
         return  cardsInHand.size();
     }
 }
