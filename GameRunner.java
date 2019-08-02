46e6ddded99ffca018b8894ef3482a0f050517e5
Daniel Brackett <gerard.brackett@gmail.com>
2019-08-02 11:14:46 -0700

    Added Java Doc Comments to methods. Finished buildingout the deal method.

--- GameRunner.java
+++ GameRunner.java
@@ -7,56 +7,78 @@ public class GameRunner {
         GameRunner game = new GameRunner();
         Deck deck = new Deck();
         deck.shuffle();
-//        game.deal(deck);
+        game.deal(deck);
 
 
         deck.printDeck();
     }
 
     private ArrayList<Player> players;
+    int numberOfPlayers;
 
+    /**
+     * Default constructor, allows for 2 'players' one dealer and one gambler.
+     */
     public GameRunner() {
         players = new ArrayList<>();
         createPlayers(2);
     }
 
-    /*
-    allows up to 5 players plus 1 dealer all players play against the dealer.
+    /**
+     * Constructor for multi-player game.
+     * allows up to 5 players plus 1 dealer all players play against the dealer.
+     * @param numberOfPlayers allows for more than the default number of players.
      */
-    public GameRunner(int players) {
-        if (players > 2 && players < 6) {
-            createPlayers(players);
+    public GameRunner(int numberOfPlayers) {
+        this.numberOfPlayers = numberOfPlayers;
+        if (numberOfPlayers > 2 && numberOfPlayers < 6) {
+            createPlayers(numberOfPlayers);
         }
     }
-    /*
-    Create the number of players of a game the game lasts until the
+
+    /**
+     * Create the number of players specified for a game.
+     * @param numberOfPlayers
      */
-    public void createPlayers(int numberOfPlayers) {
-        Player dealer = new Player();
+    private void createPlayers(int numberOfPlayers) {
+        this.numberOfPlayers = numberOfPlayers;
+        Player dealer = new Player("dealer");
         dealer.isDealer = true;
+        players.add(dealer);
         numberOfPlayers--;
         for (int i = numberOfPlayers; i > 0; i--) {
-            Player player = new Player();
+            Player player = new Player("player " + i);
             players.add(player);
         }
     }
 
     /*
-    deals the first hand to each player and the dealer.
+
      */
-//    public void deal(Deck deckInPlay) {
-//        if (deckInPlay.getDeckSize() > 3 * players.size()) {
-//            while (player.)
-//            for (Player player: players) {
-//
-//            }
-//        }
-//    }
 
-        /*
-        will continue to play with sufficient cards for all players.
+    /**
+     * deals the first hand to each player and the dealer.
+     * @param deckInPlay this is the deck currently being used to play a game.
+     */
+    public void deal(Deck deckInPlay) {
+        if (deckInPlay.getDeckSize() > (3 * players.size())) {
+            for (Player player: players) {
+                if (player.getHand().getHandSize() < 2) {
+                    //the proceeding section might benefit from a refactor.
+                    // as maybe; player.getHand().addCardToHand(deckInPlay.dealOneCardFromDeck());
+                    Card cardDealt = deckInPlay.dealOneCardFromDeck();
+                    Hand hand = player.getHand();
+                    hand.addCardToHand(cardDealt);
+                }
+            }
+        }
+    }
 
-         */
+    /**
+     * this method may be determined to be unnecessary. further work must be done.
+     * however, will continue to play with sufficient cards for all players.
+     * @return returns true when there still is a player left?
+     */
     public boolean continuePlay() {
         return true;
     }
