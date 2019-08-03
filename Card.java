c5b0d62f3fd1530aee8d15d0eb80d104a9c010f7
Daniel Brackett <gerard.brackett@gmail.com>
2019-08-02 17:58:46 -0700

    Switched up the Suit and the Card value parameterd for the constructor to utilize the new enums. Made corresponding changes to the gettters andsetters as necessary. Then made major changes to the 'ToString()' to take advantage of the enums.

--- Card.java
+++ Card.java
@@ -4,31 +4,19 @@ public class Card{
 
     /* Constructor */
 
-    public Card(String suit, int value) {
+    public Card(Suit suit, CardValueEnum cardValue) {
         this.suit = suit; //
-        this.value = value; //values are 1-13 the game should calculate the value for each card since many games
+        this.cardValue = cardValue; //values are 1-13 the game should calculate the value for each card since many games
         // assign values to cards differently.
     }
-        public String getSuit() { return suit; }
+        public Suit getSuit() { return suit; }
 
-        public int getValue() { return value; }
+        public CardValueEnum getCardValueEnum() { return cardValue; }
 
         public String ToString() {
-            if (this.getValue() >= 2 && this.getValue() <= 10) {
-                return "the " + this.getValue() + " of " + this.getSuit();
-            } else if (this.getValue() == 1) {
-                return "the Ace of " + this.getSuit();
-            } else if (this.getValue() == 11) {
-                return "the Jack of " + this.getSuit();
-            } else if (this.getValue() == 12) {
-                return "the Queen of " + this.getSuit();
-            }  else if (this.getValue() == 13) {
-                return "the King of " + this.getSuit();
-            } else {
-                return "Card Value Error. value is outside of acceptable range.";
-            }
+                return "the " + this.getCardValueEnum() + " of " + this.getSuit();
         }
 
-    private final String suit;
-    private final int value;
+    private final Suit suit;
+    private final CardValueEnum cardValue;
 }
