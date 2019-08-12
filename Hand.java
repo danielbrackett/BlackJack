70b705dcd7aed5f18db0202f74fc9c779ba92cf2
Daniel Brackett <gerard.brackett@gmail.com>
2019-08-12 15:37:27 -0700

    added some java doc lines. added a new method to supplement the 'valueOfCrdsInHand()' to ensure that the Ace can be accounted for.

--- Hand.java
+++ Hand.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.spi.CalendarDataProvider;
 
 public class Hand {
 
@@ -16,11 +17,39 @@ public class Hand {
         this.cardsInHand.clear();
     }
 
+    /**
+     * this valueOfCardsInHand is simple and known to work but does't account for the cae card and it's dual nature.
+     * added a new experimental valueOfCardsInHand2() to take into account when 1 or more ACE card forces the
+     * value of the cardsInHand over 21.
+     * @return
+     */
     public int valueOfCardsInHand() {
         int value = 0;
         for (Card card : this.cardsInHand) {
             value += card.getCardValueEnum().getCardPoints();
         }
+        if (value > 21) {
+            value = valueOfCardsInHand2(value);
+        }
+        return value;
+    }
+
+    /**
+     * This valueOfCardsInHand2 is experimental . caution when using it.
+     * @return
+     */
+    public int valueOfCardsInHand2(int originalValue) {
+        int value = 0;
+        do {
+            for (Card card : this.cardsInHand) {
+                if (card.getCardValueEnum().getCardPoints() == 11) {
+                    value += card.getCardValueEnum().getCardPoints() - 10;
+                    originalValue -= 10;
+                } else {
+                    value += card.getCardValueEnum().getCardPoints();
+                }
+            }
+        } while (originalValue > 21) ;
         return value;
     }
 
